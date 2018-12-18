2b44d62f8cd3919161b562902e64f6d7d3e55205
looooo <sppedflyer@gmail.com>
2018-12-18 06:47:42 +0100

    update to 2.15

--- recipe/build.sh
+++ recipe/build.sh
@@ -4,7 +4,8 @@ cp ${RECIPE_DIR}/Makefile_MT Makefile_MT
 
 make -f Makefile_MT \
     SPOOLES_INCLUDE_DIR="${PREFIX}/include/spooles" \
-    LIB_DIR="${PREFIX}/lib"
+    LIB_DIR="${PREFIX}/lib" \
+    VERSION="${PKG_VERSION}"
 
 cp ccx_*_MT $PREFIX/bin/ccx
 cd $SRC_DIR
