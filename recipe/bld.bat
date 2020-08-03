55a7644e0d6b4c3b08a4f605a681398c9097aab7
looooo <sppedflyer@gmail.com>
2020-08-03 13:45:11 +0200

    update win

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,12 +1,12 @@
 cd ccx*/src
-DEL Makefile_MT
-COPY %RECIPE_DIR%\Makefile_MT Makefile_MT
-COPY %BUILD_PREFIX%\Library\mingw-w64\bin\mingw32-make %BUILD_PREFIX%\Library\mingw-w64\bin\make
+del Makefile_MT
+copy %RECIPE_DIR%\Makefile_MT Makefile_MT
+copy %BUILD_PREFIX%\Library\mingw-w64\bin\mingw32-make %BUILD_PREFIX%\Library\mingw-w64\bin\make
 
 
-REM this line translates the windows-paths to paths understandable for the mingw env
-REM -m, --mixed           like --windows, but with regular slashes (C:/WINNT)
-FOR /F "delims=\" %%i IN ('cygpath.exe -m "%LIBRARY_PREFIX%"') DO set "LIBRARY_PREFIX=%%i"
+rem this line translates the windows-paths to paths understandable for the mingw env
+rem -m, --mixed           like --windows, but with regular slashes (C:/WINNT)
+for /F "delims=\" %%i IN ('cygpath.exe -m "%LIBRARY_PREFIX%"') DO set "LIBRARY_PREFIX=%%i"
 
 make -f Makefile_MT ^
     SPOOLES_INCLUDE_DIR="%LIBRARY_PREFIX%/mingw-w64/include/spooles" ^
@@ -15,6 +15,6 @@ make -f Makefile_MT ^
     FC="gfortran" ^
     VERSION="%PKG_VERSION%"
 
-REM adding .exe to make the file executable
-XCOPY ccx_*_MT "%LIBRARY_PREFIX%\bin\ccx.exe*" /Y
+rem adding .exe to make the file executable
+copy ccx_*_MT "%LIBRARY_PREFIX%\bin\ccx.exe"
 cd %SRC_DIR%
